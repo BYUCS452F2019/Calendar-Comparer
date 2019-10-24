0e4c9e95d6a97e09082062cee8caab3db4df94fe
swilkiway <swilkiway@gmail.com>
2019-10-24 10:27:33 -0600

    Added router and new calendar view

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Login from './login.js';
+import Login from './Login.js';
+import CalendarView from './Calendar.js';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-	<Login />
+	<Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Login</Link>
+            </li>
+            <li>
+              <Link to="/calendar">Calendar</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/calendar">
+            <CalendarView />
+          </Route>
+          <Route path="/">
+            <Login />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
