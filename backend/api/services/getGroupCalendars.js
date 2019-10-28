3caff2584c9d3537426b20ebea3ad2f830d82820
Jared Smith <Jared.Craig.Smith@gmail.com>
2019-10-28 09:20:20 -0600

    getGroupCalendars fix

--- backend/api/services/getGroupCalendars.js
+++ backend/api/services/getGroupCalendars.js
@@ -3,7 +3,7 @@ const group = require('../../db/models/group')
 module.exports = async function (req, res) {
     console.log("In get group calendar service")
     //the userID provided is Jared's
-    const result = await group.getGroupCalendars("026c9468-273e-4afd-bb6d-9d625d8daf9d")
+    const result = await group.getGroupCalendars(req.query.userID)
     if(result)
     {
         res.send(result)
