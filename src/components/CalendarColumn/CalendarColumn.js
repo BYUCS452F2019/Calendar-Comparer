57de35112fb30571391e2e40802af5256f9f1b9a
Jared Smith <Jared.Craig.Smith@gmail.com>
2019-12-02 16:41:36 -0700

    heat map option is avaiable if hard coded

--- src/components/CalendarColumn/CalendarColumn.js
+++ src/components/CalendarColumn/CalendarColumn.js
@@ -1,46 +1,80 @@
-import React from 'react'
-
+import React, { Component } from 'react';
 import styles from './CalendarColumn.module.css'
 
-export default function CalendarColumn({day, schedule}){
-  return (
-    <div className={styles.Container}>
-      <div className={styles.Header}>{day}</div>
-      <div className={styles.Schedule}>
-        <ScheduleItem schedule={schedule[day]} time="8:00am" />
-        <ScheduleItem schedule={schedule[day]} time="8:30am" />
-        <ScheduleItem schedule={schedule[day]} time="9:00am" />
-        <ScheduleItem schedule={schedule[day]} time="9:30am" />
-        <ScheduleItem schedule={schedule[day]} time="10:00am" />
-        <ScheduleItem schedule={schedule[day]} time="10:30am" />
-        <ScheduleItem schedule={schedule[day]} time="11:00am" />
-        <ScheduleItem schedule={schedule[day]} time="11:30am" />
-        <ScheduleItem schedule={schedule[day]} time="12:00pm" />
-        <ScheduleItem schedule={schedule[day]} time="12:30pm" />
-        <ScheduleItem schedule={schedule[day]} time="1:00pm" />
-        <ScheduleItem schedule={schedule[day]} time="1:30pm" />
-        <ScheduleItem schedule={schedule[day]} time="2:00pm" />
-        <ScheduleItem schedule={schedule[day]} time="2:30pm" />
-        <ScheduleItem schedule={schedule[day]} time="3:00pm" />
-        <ScheduleItem schedule={schedule[day]} time="3:30pm" />
-        <ScheduleItem schedule={schedule[day]} time="4:00pm" />
-        <ScheduleItem schedule={schedule[day]} time="4:30pm" />
-        <ScheduleItem schedule={schedule[day]} time="5:00pm" />
-        <ScheduleItem schedule={schedule[day]} time="5:30pm" />
-        <ScheduleItem schedule={schedule[day]} time="6:00pm" />
-        <ScheduleItem schedule={schedule[day]} time="6:30pm" />
-        <ScheduleItem schedule={schedule[day]} time="7:00pm" />
-        <ScheduleItem schedule={schedule[day]} time="7:30pm" />
-        <ScheduleItem schedule={schedule[day]} time="8:00pm" />
-        <ScheduleItem schedule={schedule[day]} time="8:30pm" />
-      </div>
-    </div>
-  )
+export class CalendarColumn extends Component {
+
+    state = {
+        schedule: this.props.schedule,
+        day: this.props.day
+    }
+
+    ScheduleItem (time) {
+
+        let calendarType = this.props.calendarType;
+        let schedule = this.state.schedule[this.state.day]
+
+        if (calendarType == "hm") //hm = heat map
+        {
+            //hsla(0-100, 82%, 49%, 1)
+            let available = schedule[time] * 100
+            const displayTime = time.match(/[0-9]+:[0-9][0-9]/)
+
+            let ItemStyles = {
+                backgroundColor: 'hsla(' + available + ', 59%, 74%, 1)'
+            };
+
+            return <div style={ItemStyles}>{displayTime}</div>
+        }
+        else if (calendarType == "mvp") {
+
+        }
+        else // calendarType == "wig(when is good)"
+        {
+            const available = schedule[time] === 1
+            const displayTime = time.match(/[0-9]+:[0-9][0-9]/)
+
+            return <div className={styles.ScheduleItem + (available ? ' ' + styles.ScheduleItemAvailable : '')}>{displayTime}</div>
+        }
+    }
+
+    render() {
+
+        return (
+            <div className={styles.Container}>
+                <div className={styles.Header}>{this.state.day}</div>
+                <div className={styles.Schedule}>
+                    {this.ScheduleItem("8:00am")}                        
+                    {this.ScheduleItem("8:30am")}
+                    {this.ScheduleItem("9:00am")}
+                    {this.ScheduleItem("9:30am")}   
+                    {this.ScheduleItem("10:00am")}
+                    {this.ScheduleItem("10:30am")}
+                    {this.ScheduleItem("11:00am")}
+                    {this.ScheduleItem("11:30am")}
+                    {this.ScheduleItem("12:00pm")}
+                    {this.ScheduleItem("12:30pm")}
+                    {this.ScheduleItem("1:00pm")}
+                    {this.ScheduleItem("1:30pm")}
+                    {this.ScheduleItem("2:00pm")}
+                    {this.ScheduleItem("2:30pm")}
+                    {this.ScheduleItem("3:00pm")}
+                    {this.ScheduleItem("3:30pm")}
+                    {this.ScheduleItem("4:00pm")}
+                    {this.ScheduleItem("4:30pm")}
+                    {this.ScheduleItem("5:00pm")}
+                    {this.ScheduleItem("5:30pm")}
+                    {this.ScheduleItem("6:00pm")}
+                    {this.ScheduleItem("6:30pm")}
+                    {this.ScheduleItem("7:00pm")}
+                    {this.ScheduleItem("7:30pm")}
+                    {this.ScheduleItem("8:00pm")}
+                    {this.ScheduleItem("8:30pm")}
+                </div>
+            </div>
+        );
+    }
+
+
 }
 
-function ScheduleItem({schedule, time}){
-  const available = schedule[time] === 1
-  const displayTime = time.match(/[0-9]+:[0-9][0-9]/)
 
-  return <div className={styles.ScheduleItem + (available?' ' + styles.ScheduleItemAvailable:'')}>{displayTime}</div>
-}
--- src/pages/Calendar/Calendar.js
+++ src/pages/Calendar/Calendar.js
@@ -2,13 +2,16 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios'
 import moment from 'moment'
 
-import CalendarColumn from '../../components/CalendarColumn/CalendarColumn'
+import { CalendarColumn } from '../../components/CalendarColumn/CalendarColumn'
 import styles from './Calendar.module.css'
 import { CalendarTypeMenu } from '../../components/CalendarTypeMenu/CalendarTypeMenu'
 
-export default function CalendarView({calendar}){
-	const [loading, setLoading] = useState(true)
-	const [schedule, setSchedule] = useState(null)
+export default function CalendarView({ calendar }) {
+    const [loading, setLoading] = useState(true)
+    const [schedule, setSchedule] = useState(null)
+
+    const [calendarType, setCalendarType] = useState("hm"); 
+
 
 	useEffect(()=>{
 		console.log('effect')
@@ -30,14 +33,14 @@ export default function CalendarView({calendar}){
 				{
 					loading
 						?<p style={{textAlign: 'center', marginTop: '20vh'}}>Loading calendar . . .</p>
-						:<div className={styles.Calendar}>
-							<CalendarColumn schedule={schedule} day="sunday" />
-							<CalendarColumn schedule={schedule} day="monday" />
-							<CalendarColumn schedule={schedule} day="tuesday" />
-							<CalendarColumn schedule={schedule} day="wednesday" />
-							<CalendarColumn schedule={schedule} day="thursday" />
-							<CalendarColumn schedule={schedule} day="friday" />
-							<CalendarColumn schedule={schedule} day="saturday" />
+                        : <div className={styles.Calendar}>
+                            <CalendarColumn schedule={schedule} day="sunday" calendarType={calendarType} />
+                            <CalendarColumn schedule={schedule} day="monday" calendarType={calendarType}  />
+                            <CalendarColumn schedule={schedule} day="tuesday" calendarType={calendarType}  />
+                            <CalendarColumn schedule={schedule} day="wednesday" calendarType={calendarType} />
+                            <CalendarColumn schedule={schedule} day="thursday" calendarType={calendarType}  />
+                            <CalendarColumn schedule={schedule} day="friday" calendarType={calendarType}  />
+                            <CalendarColumn schedule={schedule} day="saturday" calendarType={calendarType}  />
 						</div>
 				}
 			</div>
